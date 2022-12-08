ecd8a847b6e3b77f6501816970f4ab953f0ba600
Chase <chaseajarvis@gmail.com>
2022-12-08 12:35:23 -0700

    rearrange trucks to make it under 140 miles

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 import datetime
 
 
-# ARE MY DATA TYPES RIGHT???
+
 # HashTable class using chaining.
 class ChainingHashTable:
 
@@ -56,8 +56,8 @@ class Package:
 
     def __str__(self):
         return "%s, %s, %s, %s, %s, %s, %s, %s" % (
-        self.id, self.address, self.city, self.state, self.zip, self.delivery_status, self.timeLoaded,
-        self.timeDelivered)
+            self.id, self.address, self.city, self.state, self.zip, self.delivery_status, self.timeLoaded,
+            self.timeDelivered)
 
 
 # Time complexity O(n)
@@ -94,49 +94,45 @@ def deliver(truck_list, truck_time):
     total_miles = 0.0
     while truck_list:
         min_so_far = 50.0  # the minimum distance at this point
-        min_package = None  # initializing the minimum package to Nothing
+        min_package = None # initializing the minimum package to Nothing
         current_location = 0  # current location initialized to 0
         for id in truck_list:
-            package = hash_table.search(
-                id)  # iterating through hashtable, if the id is found the package is added to variable "package"
-            address = package.address
-            # address of the package object
-            index = distance_dict.get(
-                address)  # assigns the index value of address from the distance dictionary to the "index" variable
-            distance = distance_between(current_location,
-                                        index)  # utilizes the "distance_between" function to to check the distance between the current location and the next closes index
-            if min_package is None:  # if minimum package is equal to nothing
-                min_package = package  # minimum package is assigned to the value of package
-                min_so_far = distance  # minimum distance so far assigned to the distance
-            else:
-                if distance < min_so_far:  # check if distance number is less than minimum distance
-                    min_so_far = distance  # the minimum distance is equal to the distance of "distance"
-                    total_miles = total_miles + float(distance)
-                    min_package = package  # min package is assigned to none at the begginning, here it is assigned to the package id
+            package = hash_table.search(id)  # iterating through hashtable, if the id is found the package is added to variable "package"
+            address = package.address # address of the package object
+            index = distance_dict.get(address)  # assigns the index value of address from the distance dictionary to the "index" variable
+            distance = distance_between(current_location, index) # utilizes the "distance_between" function to check the distance between the current location and the next closes index
+            if min_package is None: # if minimum package is equal to nothing
+                min_so_far = distance
+                min_package = package  # minimum distance so far assigned to the distance
+            elif distance < min_so_far:  # check if distance number is less than minimum distance
+                min_so_far = distance  # the minimum distance is equal to the distance of "distance"
+                total_miles = total_miles + float(distance)
+                min_package = package  # min package is assigned to none at the beginning, here it is assigned to the package id
 
-        # time = distance/speed
         time_to_location = datetime.timedelta(minutes=((float(min_so_far) * 60) / 18))
         truck_time = (datetime.datetime.combine(datetime.datetime.today(), truck_time) + time_to_location).time()
         min_package.timeDelivered = truck_time
-        truck_list.pop()
+        truck_list.remove(int(min_package.id))
     return round(total_miles, 2), truck_time
 
 
+# TODO check formula that calculates minutes
+# TODO find out why time to location is the same for most packages
 # Time complexity O(1)
-# in a list of lists its checking the difference between loc1 and loc2,
+
 def distance_between(loc1, loc2):
     if loc1 < loc2:
         return distances[loc2][loc1]
     else:
         return distances[loc1][loc2]
 
-
+#time complexity O(n)
 def user_interface():
     while True:
         print("****************************************")
         user_input = input(
             " User Interface:\n------------------------------\n For information on ALL packages, please enter 1.\n "
-            "For information on ONE package, Please enter 2.\n "
+            "For information on ONE package, please enter 2.\n "
             "Enter 'done' to exit.\n ****************************************\n")
 
         if user_input == "done":
@@ -146,11 +142,10 @@ def user_interface():
         if user_input == '1':
 
             user_hour = int(input("Please put in hours:\n"))
-
             user_min = int(input("Please put in minutes:\n"))
 
             user_time = datetime.datetime(2022, 8, 21, user_hour, user_min).time()
-            # status = ''
+            print("ID,_____Package Address__________Delivery Deadline, Time Loaded,_Time delivered__Package Status")
             for packs in range(1, 41):
                 p = hash_table.search(packs)
                 if user_time < p.timeLoaded:
@@ -159,13 +154,11 @@ def user_interface():
                     status = 'The package has been Delivered'
                 else:
                     status = 'The package is en route'
-                print(
-                    "ID,_____Package Address__________Delivery Deadline, Time Loaded,_Time delivered__Package Status_______All trucks distance")
-                print(p, packs, status, 'All Miles travelled by trucks: ', all_miles)
+
+                print(p, packs, status)
 
         if user_input == '2':
             user_hour = int(input("Please put in hours:\n"))
-
             user_min = int(input("Please put in minutes:\n"))
 
             user_time = datetime.datetime(2022, 8, 21, user_hour, user_min).time()
@@ -181,23 +174,27 @@ def user_interface():
                 status = 'The package is en route.'
             print(
                 "ID,_____Package Address__________Delivery Deadline, ____Time Loaded,_Time delivered__Package Status_______All trucks distance")
-            print(p, status, 'All Miles travelled by trucks: ', all_miles)
+            print(p, status)
 
         if user_input not in ['1', '2', 'done']:
             print("Invalid input, Please enter 1, 2, or 'done'.")
             user_interface()
 
 
+# step 1 of the program flow is assigning the data structures and filling those data structure with the packages in the appropriate trucks.
 distance_dict = {}
 distances = []
 
-truck1_list = [2, 4, 14, 16, 13, 15, 5, 7, 8, 9, 10, 11, 12, 17, 18, 19]  # 16 truck 1 full of packages
-truck2_list = [1, 3, 6, 25, 28, 32, 36, 38, 29, 30, 31, 34, 37, 40, 20, 21]  # 16 25 and 26 are same address
-truck3_list = [22, 23, 24, 26, 27, 33, 35, 39]  # 8 31 and 32 are the same address
+# Truck assignments.
+truck1_list = [4, 14, 16, 13, 15, 5, 7, 8, 10, 11, 12, 17, 18, 19, 25, 26]  # 16 truck 1 full of packages
+truck2_list = [1, 3, 6,  28, 32, 36, 38, 29, 30, 31, 34, 37, 40, 20, 21, 2]  # 16 25 and 26 are same address
+truck3_list = [22, 23, 24, 27, 33, 35, 39, 9]  # 8 31 and 32 are the same address
 
+# loading the data from the csv files.
 load_package_data("WGUPSPackageFile.csv")
 load_distance_data('WGUPSDistanceTable.csv')
 
+# the calculation for time stamping the truck deliveries.
 total_miles1, time1_finished = deliver(truck1_list, datetime.time(8, 0))
 for p in truck2_list:
     package = hash_table.search(p)
@@ -210,8 +207,6 @@ for p in truck3_list:
 
 total_miles3, time3_finished = deliver(truck3_list, time1_finished)
 all_miles = round(total_miles1 + total_miles2 + total_miles3, 2)
-
+print('All miles traveled by all trucks: ', all_miles)
+# the last step in the program is the user interface
 user_interface()
-
-# TODO check formula that calculates minutes
-# TODO find out why time to location is the same for most packages
